5577618a9d14194519bbf81c4d87291ff69e793d
zsun365 <zsun365@gatech.edu>
2025-04-21 03:52:06 -0400

    fix

--- riskaware-app/backend/app.py
+++ riskaware-app/backend/app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import json, os
 from pathlib import Path
-load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
+load_dotenv()
 
 print("DEBUG: FIREBASE_CONFIG loaded?", "FIREBASE_CREDENTIAL_PATH" in os.environ)
 print("DEBUG: OPENAI_API_KEY loaded?", "OPENAI_API_KEY" in os.environ)
@@ -37,8 +37,19 @@ def calculate_age(birthdate, encounter_date):
     return int(today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day)))
 
 #firestore certificate json file name here, place file in same directory
-firebase_cred_path = os.environ['FIREBASE_CREDENTIAL_PATH']
-cred = credentials.Certificate(firebase_cred_path)
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
 initialize_app(cred)
 
 
